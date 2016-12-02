e7551ae5a562bedacf9dca51382b95e422b5b23d
Brian Baquiran <baquiran@pez.ai>
2016-12-02 22:50:54 +0800

    Twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,13 +14,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class GenusController extends Controller
 {
     /**
-     * @Route("/genus/{genusName}")
+     * @Route("/genus/{genusName2}")
      *
      */
     public function showAction($genusName)
     {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName
+            'name' => $genusName,
+            'notes' => $notes
         ]);
     }
 }
